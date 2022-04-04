c27bc017ec8e516aa988a029a8105d5865f56807
Miha Drofenik <miha.drofenik@visionect.com>
2022-04-04 08:46:19 +0200

    stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Mimster } from './components/memes/Memester'
-import { News } from './components/news/News'
-import { Weather } from './components/weather/Weather'
-import { useInterval } from './hooks/useInterval'
 import './index.scss'
 
 function App() {
