b8dce9ef6d87ac109787b738b8083149a7ce6094
Miha Drofenik <miha.drofenik@visionect.com>
2022-04-04 08:45:51 +0200

    only memes now!

--- src/App.tsx
+++ src/App.tsx
@@ -6,44 +6,11 @@ import { useInterval } from './hooks/useInterval'
 import './index.scss'
 
 function App() {
-  const [i, setI] = useState(2)
-
-  useInterval(() => {
-    setI((prev: number) => {
-      if (prev >= 3) {
-        return 1
-      }
-
-      return prev + 1
-    })
-  }, 20000)
-
-  switch (i) {
-    case 1:
-      return (
-        <div className="app">
-          <Mimster />
-        </div>
-      )
-    case 2:
-      return (
-        <div className="app">
-          <Weather />
-        </div>
-      )
-    case 3:
-      return (
-        <div className="app">
-          <News />
-        </div>
-      )
-    default:
-      return (
-        <div className="app">
-          <Mimster />
-        </div>
-      )
-  }
+  return (
+    <div className="app">
+      <Mimster />
+    </div>
+  )
 }
 
 export default App
